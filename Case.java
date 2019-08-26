ca231bd528cea04aef6e9f244a15712ed15a7c64
Derek H <54451223+DcaveDerps@users.noreply.github.com>
2019-08-25 21:26:00 -0400

    Commented code

--- Case.java
+++ Case.java
@@ -3,28 +3,29 @@ public class Case{
    double caseMoney = 0.0;
    boolean opened = false;
    boolean pCase = false;
+   // initialize variables
    Case(){
-   }
+   }  // default constructor
    Case(int num, double money){
       caseNum = num;
       caseMoney = money;
-   }
+   }  // initialize Case with given parameters
    public int getCaseNum(){
       return caseNum;
-   }
+   }  // return the Case's caseNum value
    public double getCaseMoney(){
       return caseMoney;
-   }
+   }  // return the Case's caseMoney value
    public boolean getState(){
       return opened;
-   }
+   }  // return the state of the opened boolean
    public boolean isPCase(){
       return pCase;
-   }
+   }  // return the state of the pCase boolean
    public void openCase(){
       opened = true;
-   }
+   }  // function to set the state of the case to "opened"
    public void pickedAsPlayer(){
       pCase = true;
-   }
+   }  // function to set the case as being originally chosen by the player
 }
